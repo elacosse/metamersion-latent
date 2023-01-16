849437043ae47fad99741c5a528e7d4836acdcda
elacosse <eric.lacosse@research.fchampalimaud.org>
2023-01-16 10:24:15 -0600

    some audio function to manage concatenation.

---
+++ metamersion_latent/audio/tts.py
@@ -0,0 +1,60 @@
+import wave
+
+import numpy as np
+
+
+def audio_length(file_path):
+    """Get the length of an audio file in seconds.
+    Args:
+        file_path (str): Path to audio file.
+    Returns:
+        float: Length of audio file in seconds.
+    """
+    with wave.open(file_path, "r") as audio:
+        frames = audio.getnframes()
+        rate = audio.getframerate()
+    return frames / float(rate)
+
+
+def assemble_audio_files(file_paths, silence_duration, output_file):
+    """Assemble audio files into a single audio file with silence between each file.
+    Args:
+        file_paths (list): List of paths to audio files.
+        silence_duration (float): Duration of silence between audio files.
+        output_file (str): Path to output file.
+    """
+    # Open the first audio file
+    with wave.open(file_paths[0], "rb") as audio_file:
+        # Get the audio parameters
+        sample_rate = audio_file.getframerate()
+        sample_width = audio_file.getsampwidth()
+        channels = audio_file.getnchannels()
+        # Create a numpy array to hold the audio data
+        audio_data = np.frombuffer(audio_file.readframes(-1), np.int16)
+        # Iterate through the remaining audio files
+        for file_path in file_paths[1:]:
+            # Open the next audio file
+            with wave.open(file_path, "rb") as next_audio_file:
+                # Check that the audio parameters match
+                assert next_audio_file.getframerate() == sample_rate
+                assert next_audio_file.getsampwidth() == sample_width
+                assert next_audio_file.getnchannels() == channels
+                # Read the audio data
+                next_audio_data = np.frombuffer(
+                    next_audio_file.readframes(-1), np.int16
+                )
+                # Create silence data
+                silence_data = np.zeros(int(sample_rate * silence_duration), np.int16)
+                # Concatenate the audio data and silence
+                audio_data = np.concatenate((audio_data, silence_data, next_audio_data))
+    # Open the output file for writing
+    with wave.open(output_file, "wb") as output_audio_file:
+        # Set the audio parameters
+        output_audio_file.setframerate(sample_rate)
+        output_audio_file.setsampwidth(sample_width)
+        output_audio_file.setnchannels(channels)
+        # Write the audio data to the output file
+        output_audio_file.writeframes(
+            audio_data.tobytes()
+        )  # Write the audio data to the output file
+        output_audio_file.writeframes(audio_data.tobytes())
