4a9cbcba6ca86a694dc21a0c47d7b551a2afc16e
Johannes Stelzer <mail@johannes-stelzer.de>
2023-01-21 17:32:40 +0100

    bugfix

--- metamersion_latent/image_generation/install_dependencies.sh
+++ metamersion_latent/image_generation/install_dependencies.sh
@@ -40,4 +40,4 @@ pip install PyVimeo
 wget https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt v2-1_768-ema-pruned.ckpt 
 
 
-
+python first_run.py
--- metamersion_latent/image_generation/scp_to_remote.sh
+++ metamersion_latent/image_generation/scp_to_remote.sh
@@ -2,7 +2,7 @@
 # supply as first argument the IP of the remote server
 dp_remote="/home/ubuntu/"
 user=ubuntu
-scp install_dependencies.sh $user@$1:/$dp_remote/
 scp run_remote_server.py $user@$1:/$dp_remote/
+scp install_dependencies.sh $user@$1:/$dp_remote/
 
 echo "scp'd everything."
