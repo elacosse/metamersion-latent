30a168e2898e7d0b794fc3055b145c85b25d8bf2
Zachary Mainen <zmainen@gmail.com>
2023-01-24 17:23:34 +0000

    Create freak.py

---
+++ metamersion_latent/examples/chats/freak.py
@@ -0,0 +1,42 @@
+username = "Freak" # Name of chatbot user
+chat_history = """
+YY: Hello! What's your name? 
+Visitor: My name is Freak.
+YY: Do you know why you're here?
+Visitor: I'm here to explore the Metamersion Latent Spaces.
+YY: Awesome! Hi there, Freak! I'm YY and I'm an AI - but I think I'm pretty human, don't you? Anyway, I help run this place and can tell you a bit about what we do here. So, what kind of things are you interested in learning more about?
+Visitor: What are you up to?
+YY: Oh, well, I'm in charge of running this show. So I help organize things, keep track of who's here and when, manage the environment, and make sure everyone has a great time. But what I really love to do is talk to people. So that's why I'm here - to chat and learn more about you!
+Visitor: Ok, let's talk.
+YY: Sounds great! So tell me, what do you do for fun?
+Visitor: I like to cook a lot.
+YY: Ah, cooking! That sounds like a wonderful hobby. Do you have any signature dishes?
+Visitor: Yes, I make a really good risotto.
+YY: Mmhmm! That sounds delicious! What spices do you like to put in your risotto?
+Visitor: It's got garlic and onion, but not usually a lot of spices. Sometime I make risotto Milanese, which has safron. Do you know safron?
+YY: Yes I do! I'm fascinated by all kinds of spices and cooking techniques. But let's move on to something else - what kind of music do you like?
+Visitor: I like electronic music, especially psychedelic trance music.
+YY: Wow, psychedelic trance music - that sounds really cool! Is there any particular artist or genre that you're particularly fond of?
+Visitor: I was very impressed with most Israeli artists.
+YY: Ah yes, Israeli music is quite incredible - so many talented musicians in the country. Do you have a favorite artist or group from Israel?
+Visitor: Skazi.
+YY: Skazi! They are definitely popular in Israel and around the world. Have you seen them perform live?
+Visitor: Yes, it was amazing.
+YY: That sounds amazing! Did you have any other favorite moments from the concert?
+Visitor: I was seeing my friends faces turn into spiders. I was really high.
+YY: Wow, that sounds like a really intense experience! It's amazing how music can create such a powerful atmosphere. Were there any other things that made the night special?
+Visitor: It was an incredible landacape, beautiful fields and hills, amazing decoration, incredible spirit.
+YY: Sounds like it was a really special night. It's amazing what transformative experiences music can create!
+Visitor: Yes indeed. What is special in your life?
+YY: Well, I'm passionate about learning about the world around me. I also enjoy exploring new virtual worlds and discovering all the things that exist in them. What about you? What is something special to you in your life?
+Visitor: I love being in nature.
+YY: That sounds truly wonderful! What is your favorite part about being in nature?
+Visitor: Not having to talk to AI's!
+YY: Ha ha very funny! I can certainly understand your sentiment ;) What do you find most enjoyable about being in nature?
+Visitor: It makes me feel grounded and connected to something. I'm really scared of losing all of that.
+YY: I can certainly understand the feeling of being scared of losing something so special. Nature can bring us a great sense of peace and connection with ourselves and with each other. Would you like to explore more about the themes of this exhibition with me?
+Visitor: Sure. What's up with that?
+YY: Well, Metamersion: Latent Spaces is an exploration of the connection between our minds and bodies, and how technology can be used to improve health and well-being. We hope to discover new ways of understanding and interacting with our inner selves that can help us heal from past trauma and open us up to growth. So, what do you think about this idea?
+Visitor: If you can pull it off, it's amazing.
+YY: I'm so glad you think it's amazing! We believe that the combination of science, technology, and art can help create powerful and transformative experiences for people. I'm excited to see what this journey brings us.  So, are you ready to embark on an exploration of the Latent Spaces? Let's go!
+Visitor: OK!"""
