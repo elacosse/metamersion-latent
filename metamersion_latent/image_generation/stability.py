32d0bf7259932dfa742ed936f506b21aa622acee
elacosse <eric.lacosse@research.fchampalimaud.org>
2023-01-24 15:10:56 -0600

    rid of stability api

--- metamersion_latent/image_generation/stability.py
+++ metamersion_latent/image_generation/stability.py
@@ -1,52 +1,50 @@
-import io
-import os
-import warnings
 from pathlib import Path
 
-import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+# import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 from PIL import Image, ImageDraw, ImageFont
-from stability_sdk import client
+
+# from stability_sdk import client
 
 
-def generate_images_from_prompts(prompts: list, config: dict) -> list:
-    """Generate images from prompts.
-    Args:
-        prompts (list): A list of prompts.
-    Returns:
-        list: A list of images.
-    """
-    stability_api = client.StabilityInference(
-        key=os.environ["STABILITY_KEY"],
-        engine=config["engine"],
-        verbose=True,
-    )
+# def generate_images_from_prompts(prompts: list, config: dict) -> list:
+#     """Generate images from prompts.
+#     Args:
+#         prompts (list): A list of prompts.
+#     Returns:
+#         list: A list of images.
+#     """
+#     stability_api = client.StabilityInference(
+#         key=os.environ["STABILITY_KEY"],
+#         engine=config["engine"],
+#         verbose=True,
+#     )
 
-    images = []
-    for i, prompt in enumerate(prompts):
-        # the object returned is a python generator
-        answers = stability_api.generate(
-            prompt=prompt,
-            seed=config["seed"],
-            steps=config["steps"],
-            cfg_scale=config["cfg_scale"],
-            width=config["width"],
-            height=config["height"],
-            sampler=config["sampler"],
-            guidance_preset=config["guidance_preset"],
-            safety=config["safety"],
-        )
-        # iterating over the generator produces the api response
-        for j, resp in enumerate(answers):
-            for k, artifact in enumerate(resp.artifacts):
-                if artifact.finish_reason == generation.FILTER:
-                    warnings.warn(
-                        "Your request activated the API's safety filters and could not be processed."
-                        "Please modify the prompt and try again."
-                    )
-                if artifact.type == generation.ARTIFACT_IMAGE:
-                    img = Image.open(io.BytesIO(artifact.binary))
-                    images.append(img)
-    return images
+#     images = []
+#     for i, prompt in enumerate(prompts):
+#         # the object returned is a python generator
+#         answers = stability_api.generate(
+#             prompt=prompt,
+#             seed=config["seed"],
+#             steps=config["steps"],
+#             cfg_scale=config["cfg_scale"],
+#             width=config["width"],
+#             height=config["height"],
+#             sampler=config["sampler"],
+#             guidance_preset=config["guidance_preset"],
+#             safety=config["safety"],
+#         )
+#         # iterating over the generator produces the api response
+#         for j, resp in enumerate(answers):
+#             for k, artifact in enumerate(resp.artifacts):
+#                 if artifact.finish_reason == generation.FILTER:
+#                     warnings.warn(
+#                         "Your request activated the API's safety filters and could not be processed."
+#                         "Please modify the prompt and try again."
+#                     )
+#                 if artifact.type == generation.ARTIFACT_IMAGE:
+#                     img = Image.open(io.BytesIO(artifact.binary))
+#                     images.append(img)
+#     return images
 
 
 def create_collage(images: list) -> Image:
@@ -95,4 +93,5 @@ def write_text_under_image(image, text_list):
         draw.text(
             (0 + spacing * i, height + 10), text_list[i], font=font, fill=(0, 0, 0)
         )
+    return new_image
     return new_image
