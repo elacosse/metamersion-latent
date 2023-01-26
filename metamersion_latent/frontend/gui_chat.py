c8f7d30c88229ee56ff78eca30aaf488d780e138
lunar <stelzer@lunar-ring.ai>
2023-01-26 18:16:36 +0000

    chat defaults

--- metamersion_latent/frontend/gui_chat.py
+++ metamersion_latent/frontend/gui_chat.py
@@ -737,7 +737,7 @@ if __name__ == "__main__":
     # Change Parameters below
 
     parser = argparse.ArgumentParser(description="ChatGUI")
-    parser.add_argument("--fp_config", type=str, default="../configs/chat/lfs1_version_4.2oldintro.py")
+    parser.add_argument("--fp_config", type=str, default="../configs/chat/ls1_version_4.2oldintro.py")
     parser.add_argument("--verbose_ai", type=bool, default=True)
     parser.add_argument("--portugese_mode", type=bool, default=False)
     parser.add_argument("--ai_fake_typing", type=bool, default=True)
